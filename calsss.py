609c1145e72b9d8a836340ac5bcbf50226b89362
navid <navid.sahleh@gmail.com>
2020-04-19 00:48:45 +0430

    files added

---
+++ calsss.py
@@ -0,0 +1,224 @@
+import pickle
+
+class Student(object):
+    def __init__(self,name,code):
+        self.name = name
+        self.code = code
+        
+    '''def edit_student(self,record):
+        with open('navid.txt','r+') as f:
+            lines=f.readlines()
+            for i in range(0,len(lines)):
+                if record in lines[i]:
+                    k=input("give the new name: ")
+                    p=lines[i].split(" ")
+                    lines[i]=k+" "+p[1]
+                    break
+            f.seek(0)
+            f.writelines(lines)'''
+      
+#    def neshon(self):
+#        with open('navid.txt', "rb") as f:
+#            while True:
+#                try:
+#                    yield pickle.load(f)
+#                except EOFError:
+#                    break
+            
+            
+    def edit_student(self,p,record):
+        for i in p:
+            if i.code==record:
+                k=input("give the new name: ")
+                i.name=k
+                break
+        with open('navid.txt','wb') as f:
+            pickle.dump(p,f, protocol=pickle.HIGHEST_PROTOCOL)
+        
+        
+    
+    
+                
+    def get_name(self):
+        print(self.name)
+    
+    
+class Manager(object):
+    
+    def __init__(self,name):
+        self.name=name
+        self.stu = []
+            
+    def edit_student(self,p,record):
+        Student.edit_student(self,p,record)
+        
+    def show_member(self,room):
+        room.show_member()
+        
+    '''def add(self,person):
+        name=person.name
+        record=person.code
+        with open('navid.txt','a+') as f:
+            f.write(name)
+            f.write(' '+str(record))
+            f.write('\n')'''
+            
+     #jagozin commente        
+    def add(self,person):
+        with open('navid.txt','ab') as f:
+            pickle.dump(person,f, protocol=pickle.HIGHEST_PROTOCOL)
+#            f.write('\n')
+        
+
+    def load(self):
+        with open('navid.txt', "rb") as f:
+            while True:
+                try:
+                    yield pickle.load(f)
+                except EOFError:
+                    break
+            
+
+    
+    def delete_student(self,record):
+        with open("navid.txt", "r") as f:
+            lines = f.readlines()
+            with open("navid.txt", "w") as f:
+                for line in lines:
+                    if record not in line: 
+                        f.write(line)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    def add_stu(self,student):
+        self.stu.append(student)
+        print(self.stu)
+        
+        
+    def find_stu(self,record):
+        for Student in self.stu:
+            if record==Student.code:
+                return Student
+
+    
+class Room(object):
+    def __init__(self,roomnum,capacity,price,floornum):
+        self.roomnum = roomnum
+        self.capacity = capacity
+        self.price = price
+        self.floornum = floornum
+        self.member = []
+        
+        
+    def add_member(self,student):
+        if len(self.member)<int(self.capacity):
+            self.member.append(student)
+            print(self.member)
+        else:
+            print("the room is full!")
+        
+    def show_member(self):
+        for Student in self.member:
+            Student.get_name()
+            
+    def delete_member(self,name):
+        for Student in self.member:
+            if name== Student.get_name():
+                self.member.remove(Student)
+        
+            
+'''
+he = Student('vahid','12467')
+she = Student('navid','23252')
+manager = Manager('kasra')
+room = Room('1','4','12','1')
+room.add_member(he)
+room.add_member(she)
+manager.show_member(room)'''
+
+
+
+#he = Student('vahid','12467')
+#she = Student('navid','23252')
+manager = Manager('kasra')
+record = "23252"
+#manager.add(he)
+#manager.add(she)
+
+###### edit kardane daneshjo ###############
+he = Student('vahid','12467')
+she = Student('navid','23252')
+manager = Manager('kasra')
+record = "23252"
+manager.add(he)
+manager.add(she)
+p=list(manager.load())
+manager.edit_student(p,record)
+#############################################
+
+
+
+
+
+'''
+manager = Manager("MR rahmani")
+print("Welcome",manager)            
+print("what do you want to do?"+"\n1"+")"+"New student"+"\n2"+")"+"Register new block"+
+    "\n3"+")"+"Register new room"+"\n4"+")"+"edit Student")
+ask=int(input())
+
+#add new Student
+if ask==1:
+    
+    name=input("give me the name of student:\n")
+    record=input("give me the record of student\n")
+    he = Student(name,record)
+    
+    manager.add(he)
+    del name
+
+elif ask==3:
+    num=input("Please enter a Number for room:\n")
+    cap=input("Please enter the Capicity of room:\n")
+    price=input("Please enter the rent price for room\n")
+    floor=input("Please enter the floor number:\n")
+    room = Room(num,cap,price,floor)
+    
+    print("what do you want to do?"+"\n1"+")"+"Add member"+"\n2"+")"+"Delete member")
+    soal=input()
+    
+    if soal=="1":
+        name=input("give me the name of student:\n")
+        record=input("give me the record of student\n")
+        he = Student(name,record)
+        room.add_member(he)
+        
+#    elif soal=="2":
+        
+
+
+elif ask==4:
+    
+    record=input("Please enter Student Number:\n")
+    print("\n1"+")"+"Delete"+"\n2"+")"+"Edit"+"\n3"+")"+"Chosse room"+"\n4"+")"+"Show room ")
+    give=int(input())
+    
+    
+    #delete Student
+    if give==1:
+        
+        manager.delete_student(record)
+        del record
+        
+    #edit Student name
+    elif give==2:
+        manager.edit_student(record)
+        del record 
+
+'''
+
+
+
+
